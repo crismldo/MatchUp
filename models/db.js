758f9cfe1ccaff21784a51a6513f6101f0cc8820
Cris Maldonado <cristinamldo05@gmail.com>
2025-10-21 19:23:54 -0600

    Quitar pools

--- models/db.js
+++ models/db.js
@@ -1,22 +1,21 @@
 const mysql = require('mysql2');
 
-// Crear un pool de conexiones
-const pool = mysql.createPool({
+// Configuración de la conexión
+
+const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-  waitForConnections: true,
-  connectionLimit: 10, // número máximo de conexiones simultáneas
-  queueLimit: 0
+  port: process.env.MYSQLPORT
 });
 
-// Exportar versión con promesas (para usar async/await)
-const db = pool.promise();
-
-db.getConnection()
-  .then(() => console.log('Conectado a la base de datos MySQL de Railway'))
-  .catch(err => console.error('Error al conectar con la DB:', err));
+db.connect(err => {
+  if (err) {
+    console.error('Error al conectar con la DB:', err);
+  } else {
+    console.log('Conectado a la base de datos MySQL de Railway');
+  }
+});
 
 module.exports = db;
