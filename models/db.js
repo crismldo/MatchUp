909252eae8723ef89bd5f67fb59372f959009880
Cris Maldonado <cristinamldo05@gmail.com>
2025-10-21 18:26:13 -0600

    Cambio a conexiones pool

--- models/db.js
+++ models/db.js
@@ -1,21 +1,22 @@
 const mysql = require('mysql2');
 
-// Configuración de la conexión
-
-const db = mysql.createConnection({
+// Crear un pool de conexiones
+const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10, // número máximo de conexiones simultáneas
+  queueLimit: 0
 });
 
-db.connect(err => {
-  if (err) {
-    console.error('Error al conectar con la DB:', err);
-  } else {
-    console.log('Conectado a la base de datos MySQL de Railway');
-  }
-});
+// Exportar versión con promesas (para usar async/await)
+const db = pool.promise();
+
+db.getConnection()
+  .then(() => console.log('Conectado a la base de datos MySQL de Railway'))
+  .catch(err => console.error('Error al conectar con la DB:', err));
 
 module.exports = db;
